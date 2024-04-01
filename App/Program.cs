d759f13468421fe18e09663ec258dae3a3763630
Xion <xionwin@hotmail.com>
2024-04-02 01:17:03 +0800

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -10,7 +10,8 @@ class Program
         using (var window = new KMSWindow())
         window.Run();
 #else
-        using (var window = new Window(800, 480))
+        // using (var window = new Window(800, 480))
+        using (var window = new Window(1280, 1024))
             window.Run();
 #endif
     }
